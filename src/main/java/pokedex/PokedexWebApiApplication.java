ae9e961c0f0cc68283ef2dc74a7276ffa6241723
snigelkott <dahlstedt.per@gmail.com>
2020-10-21 12:46:11 +0200

    Initial commit

---
+++ src/main/java/pokedex/PokedexWebApiApplication.java
@@ -0,0 +1,13 @@
+package pokedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokedexWebApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pokedex/PokedexWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pokedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
