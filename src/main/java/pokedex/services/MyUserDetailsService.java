388498796db951b4429180d4d44a4b28a5d39c4e
snigelkott <dahlstedt.per@gmail.com>
2020-10-30 15:01:39 +0100

    Added custom user details service

--- src/main/java/pokedex/services/MyUserDetailsService.java
+++ src/main/java/pokedex/services/MyUserDetailsService.java
@@ -1,4 +1,34 @@
 package pokedex.services;
 
-public class MyUserDetailsService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import pokedex.entities.User;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userService.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("Username " + username + " not found.");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), getGrantedAuthorities(user));
+    }
+
+    private Collection<GrantedAuthority> getGrantedAuthorities(User user) {
+        return user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toList());
+    }
 }
