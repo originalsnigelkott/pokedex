75ce9eac14ff13a2f7c3e54373f7d2b5bb9e30c2
snigelkott <dahlstedt.per@gmail.com>
2020-10-30 15:00:35 +0100

    Added user service

--- src/main/java/pokedex/services/UserService.java
+++ src/main/java/pokedex/services/UserService.java
@@ -1,4 +1,18 @@
 package pokedex.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pokedex.entities.User;
+import pokedex.exceptions.EntityNotFoundException;
+import pokedex.repositories.UserRepository;
+
+@Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User findByUsername(String username) {
+        var user = userRepository.findByUsername(username).orElseThrow(() -> new EntityNotFoundException("user", "username"));
+        return user;
+    }
 }
