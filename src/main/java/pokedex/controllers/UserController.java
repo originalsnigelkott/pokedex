03aa5d01e37db903502e025782192b9cd167adae
snigelkott <dahlstedt.per@gmail.com>
2020-11-01 11:04:03 +0100

    Added findById endpoint

--- src/main/java/pokedex/controllers/UserController.java
+++ src/main/java/pokedex/controllers/UserController.java
@@ -4,13 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import pokedex.entities.User;
 import pokedex.services.UserService;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/api/v1/users")
 public class UserController {
     @Autowired
     private UserService userService;
@@ -20,5 +22,8 @@ public class UserController {
         return ResponseEntity.ok(userService.findAll());
     }
 
-
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable String id) {
+        return ResponseEntity.ok(userService.findById(id));
+    }
 }
