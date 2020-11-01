dbe7fefb3367e8fc110bbcdb6e99b390b07353a9
snigelkott <dahlstedt.per@gmail.com>
2020-11-01 12:17:51 +0100

    Added role requirements to endpoints

--- src/main/java/pokedex/controllers/UserController.java
+++ src/main/java/pokedex/controllers/UserController.java
@@ -3,6 +3,7 @@ package pokedex.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import pokedex.entities.User;
@@ -12,9 +13,9 @@ import java.net.URI;
 import java.util.List;
 
 @Controller
-@RequestMapping("/api/v1/users/")
+@RequestMapping("/api/v1/users")
 public class UserController {
-    private final String ENDPOINT_NAME = "/api/v1/users";
+    private final String ENDPOINT_NAME = "/api/v1/users/";
 
     @Autowired
     private UserService userService;
@@ -27,22 +28,26 @@ public class UserController {
     }
 
     @GetMapping
+    @Secured("ROLE_ADMIN")
     public ResponseEntity<List<User>> find() {
         return ResponseEntity.ok(userService.findAll());
     }
 
     @GetMapping("/{id}")
+    @Secured({"ROLE_ADMIN", "ROLE_MEMBER"})
     public ResponseEntity<User> findById(@PathVariable String id) {
         return ResponseEntity.ok(userService.findById(id));
     }
 
     @PutMapping("/{id}")
+    @Secured({"ROLE_ADMIN", "ROLE_MEMBER"})
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void update(@PathVariable String id, @RequestBody User user) {
         userService.update(id, user);
     }
 
     @DeleteMapping("/{id}")
+    @Secured("ROLE_ADMIN")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable String id) {
         userService.delete(id);
