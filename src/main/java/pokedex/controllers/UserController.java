d9ffabc6dd3977b9037af4352d01d1bb4783aadd
snigelkott <dahlstedt.per@gmail.com>
2020-11-01 11:29:51 +0100

    Added put endpoint

--- src/main/java/pokedex/controllers/UserController.java
+++ src/main/java/pokedex/controllers/UserController.java
@@ -1,6 +1,7 @@
 package pokedex.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -33,5 +34,11 @@ public class UserController {
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable String id) {
         return ResponseEntity.ok(userService.findById(id));
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@PathVariable String id, @RequestBody User user) {
+        userService.update(id, user);
     }
 }
