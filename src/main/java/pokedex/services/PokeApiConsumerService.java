3f69c36de8248029f7bd8b269cdd80bf67e6a2cf
snigelkott <dahlstedt.per@gmail.com>
2020-11-04 17:30:45 +0100

    Added get type method

--- src/main/java/pokedex/services/PokeApiConsumerService.java
+++ src/main/java/pokedex/services/PokeApiConsumerService.java
@@ -4,24 +4,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 import pokedex.dtos.pokemon.PokemonDto;
+import pokedex.dtos.type.TypeDto;
 import pokedex.entities.Pokemon;
+import pokedex.entities.Type;
+import pokedex.exceptions.EntityNotFoundException;
+import pokedex.exceptions.PokeApiException;
 import pokedex.repositories.PokeApiResourceRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class PokemonConsumerService {
+public class PokeApiConsumerService {
     @Autowired
     private PokeApiResourceRepository pokeApiResourceRepository;
 
-    private String baseUrl = "https://pokeapi.co/api/v2/pokemon/";
+    private String baseUrl = "https://pokeapi.co/api/v2/";
     private final RestTemplate restTemplate;
 
-    public PokemonConsumerService(RestTemplateBuilder restTemplateBuilder) {
+    public PokeApiConsumerService(RestTemplateBuilder restTemplateBuilder) {
         restTemplate = restTemplateBuilder.build();
     }
 
@@ -34,12 +40,31 @@ public class PokemonConsumerService {
     }
 
     private Pokemon getOnePokemonByName(String name) {
-        var url = baseUrl + name;
+        var url = baseUrl + "pokemon/" + name;
         try {
             var pokemonDto = restTemplate.getForObject(url, PokemonDto.class);
             return new Pokemon(pokemonDto);
+        } catch (HttpServerErrorException e) {
+            throw new PokeApiException();
         } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE, "Failed to fetch pokemon.");
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error.");
+        }
+    }
+
+    public Type getTypeByName(String name) {
+        var url = baseUrl + "type/" + name;
+        try {
+            var typeDto = restTemplate.getForObject(url, TypeDto.class);
+            return new Type(typeDto);
+        } catch (HttpServerErrorException e) {
+            throw new PokeApiException();
+        } catch (HttpClientErrorException e) {
+            if(e.getStatusCode() == HttpStatus.NOT_FOUND) {
+                throw new EntityNotFoundException("type", "name");
+            }
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error.");
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error.");
         }
     }
 }
