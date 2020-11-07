b2dc86f9eb5360599b715406922d38a251de8cbe
Per <dahlstedt.per@gmail.com>
2020-11-07 12:20:52 +0100

    Added documentation

--- src/main/java/pokedex/controllers/AbilityController.java
+++ src/main/java/pokedex/controllers/AbilityController.java
@@ -1,5 +1,11 @@
 package pokedex.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,11 +20,29 @@ public class AbilityController {
     @Autowired
     private AbilityService abilityService;
 
+    @Operation(summary = "Find ability by id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found matching ability.",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Ability.class))}),
+            @ApiResponse(responseCode = "404", description = "Did not find any ability.",
+                    content = @Content)})
     @GetMapping("/{id}")
     public ResponseEntity<Ability> findById(@PathVariable String id) {
         return ResponseEntity.ok(abilityService.findById(id));
     }
 
+    @Operation(summary = "Finds abilities matching the request.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found matching abilities.",
+                    content = {@Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = Ability.class)))}),
+            @ApiResponse(responseCode = "400", description = "Invalid request parameters.",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Did not find any ability matching name. A full match with name is required",
+                    content = @Content),
+            @ApiResponse(responseCode = "503", description = "Third party service is not available.")
+    })
     @GetMapping
     public ResponseEntity<List<Ability>> findAll(@RequestParam(required = false) String name) {
         return ResponseEntity.ok(abilityService.find(name));
--- src/main/java/pokedex/controllers/MoveController.java
+++ src/main/java/pokedex/controllers/MoveController.java
@@ -1,8 +1,15 @@
 package pokedex.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pokedex.entities.Ability;
 import pokedex.entities.Move;
 import pokedex.services.MoveService;
 
@@ -14,11 +21,29 @@ public class MoveController {
     @Autowired
     private MoveService moveService;
 
+    @Operation(summary = "Find move by id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found matching move.",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Move.class))}),
+            @ApiResponse(responseCode = "404", description = "Did not find any move.",
+                    content = @Content)})
     @GetMapping("/{id}")
     public ResponseEntity<Move> findById(@PathVariable String id) {
         return ResponseEntity.ok(moveService.findById(id));
     }
 
+    @Operation(summary = "Finds moves matching the request.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found matching moves.",
+                    content = {@Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = Move.class)))}),
+            @ApiResponse(responseCode = "400", description = "Invalid request parameters.",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Did not find any move matching name. A full match with name is required",
+                    content = @Content),
+            @ApiResponse(responseCode = "503", description = "Third party service is not available.")
+    })
     @GetMapping
     public ResponseEntity<List<Move>> findAll(@RequestParam(required = false) String name) {
         return ResponseEntity.ok(moveService.find(name));
