4f45d6405bf7a337ae54320b8b0988313c3a8cc5
Per <dahlstedt.per@gmail.com>
2020-11-07 11:29:51 +0100

    Added get ability and refactored

--- src/main/java/pokedex/services/PokeApiConsumerService.java
+++ src/main/java/pokedex/services/PokeApiConsumerService.java
@@ -8,8 +8,10 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
+import pokedex.dtos.ability.AbilityDto;
 import pokedex.dtos.pokemon.PokemonDto;
 import pokedex.dtos.type.TypeDto;
+import pokedex.entities.Ability;
 import pokedex.entities.Pokemon;
 import pokedex.entities.Type;
 import pokedex.exceptions.EntityNotFoundException;
@@ -21,6 +23,9 @@ import java.util.List;
 
 @Service
 public class PokeApiConsumerService {
+    private final String typeEntity = "type";
+    private final String abilityEntity = "ability";
+
     @Autowired
     private PokeApiResourceRepository pokeApiResourceRepository;
 
@@ -52,19 +57,42 @@ public class PokeApiConsumerService {
     }
 
     public Type getTypeByName(String name) {
-        var url = baseUrl + "type/" + name;
+        return (Type)getEntityByName(name, typeEntity, TypeDto.class);
+    }
+
+    public Ability getAbilityByName(String name) {
+        return (Ability)getEntityByName(name, abilityEntity, AbilityDto.class);
+    }
+
+    private Object getEntityByName(String name, String entity, Class dtoClass) {
+        var url = baseUrl + entity + "/" + name;
         try {
-            var typeDto = restTemplate.getForObject(url, TypeDto.class);
-            return new Type(typeDto);
+            var dto = restTemplate.getForObject(url, dtoClass);
+            return getNewEntity(entity, dto);
         } catch (HttpServerErrorException e) {
             throw new PokeApiException();
         } catch (HttpClientErrorException e) {
             if(e.getStatusCode() == HttpStatus.NOT_FOUND) {
-                throw new EntityNotFoundException("type", "name");
+                throw new EntityNotFoundException(entity, "name");
             }
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error.");
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error.");
+        }
+
+    }
+
+    private Object getNewEntity(String entity, Object dto) {
+        switch (entity) {
+            case typeEntity: {
+                return new Type((TypeDto)dto);
+            }
+            case abilityEntity: {
+                return new Ability((AbilityDto)dto);
+            }
+            default: {
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error.");
+            }
         }
     }
 }
